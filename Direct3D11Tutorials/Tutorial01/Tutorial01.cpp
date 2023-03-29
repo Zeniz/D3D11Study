c65bbc5be57726c45ab1c435b6fe072e690a227d
Zeniz <zeniz.sians@gmail.com>
2023-03-30 02:14:09 +0900

    Tutorial1 cpp

--- Direct3D11Tutorials/Tutorial01/Tutorial01.cpp
+++ Direct3D11Tutorials/Tutorial01/Tutorial01.cpp
@@ -27,9 +27,9 @@ HINSTANCE               g_hInst = nullptr;      // Global Handle Instance.
 HWND                    g_hWnd = nullptr;       // Global Handle Window.
 D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
 D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device*           g_pd3dDevice = nullptr;
+ID3D11Device*           g_pd3dDevice = nullptr;             // Device: 렌더링에 필요한 리소스들(버퍼, 텍스쳐, 버텍스세이더 등)을 생성하기 위한 인터페이스 입니다.
 ID3D11Device1*          g_pd3dDevice1 = nullptr;
-ID3D11DeviceContext*    g_pImmediateContext = nullptr;
+ID3D11DeviceContext*    g_pImmediateContext = nullptr;      // Context: Device가 소유한 리소스를 사용하여, 어떤 파이프라인 상태에서 / 어떤 렌더링 커맨드를 내리기 위한 인터페이스입니다.
 ID3D11DeviceContext1*   g_pImmediateContext1 = nullptr;
 IDXGISwapChain*         g_pSwapChain = nullptr;
 IDXGISwapChain1*        g_pSwapChain1 = nullptr;
@@ -59,7 +59,7 @@ int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     if( FAILED( InitWindow( hInstance, nCmdShow ) ) )
         return 0;
 
-    // DX 구성.
+    // DX Device 구성. Body를 생성한다.
     if( FAILED( InitDevice() ) )
     {
         CleanupDevice();
@@ -159,19 +159,55 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam 
 //--------------------------------------------------------------------------------------
 // Create Direct3D device and swap chain
 //--------------------------------------------------------------------------------------
+// 1. Device 생성: 사용할 레이어등에 관한 Flag와 드라이버에 맞는 Device 생성
+// 2. SwapChain 생성: DXGI_SWAP_CHAIN_DESC 에 명시하여 생성.
+// 3. ImmediateContext 설정: Device 생성 시 획득한 ImmediateContext에 렌더타겟과 뷰포트 설정.
 HRESULT InitDevice()
 {
     HRESULT hr = S_OK;
-
+    
+    // 먼저 생성해둔 윈도우의 Rect.
     RECT rc;
-    GetClientRect( g_hWnd, &rc );
+    GetClientRect( g_hWnd, &rc );          
     UINT width = rc.right - rc.left;
     UINT height = rc.bottom - rc.top;
 
+    // 1. Device 생성: 사용할 레이어등에 관한 Flag와 드라이버에 맞는 Device 생성
+    
+    // D3D Device에서 사용될 레이어의 플래그를 설정
     UINT createDeviceFlags = 0;
+    /*
+    typedef enum D3D11_CREATE_DEVICE_FLAG {
+        D3D11_CREATE_DEVICE_SINGLETHREADED = 0x1,                                   // App이 싱글스레드로 동작하는 경우, 성능을 향상시킬 수 있습니다.
+        D3D11_CREATE_DEVICE_DEBUG = 0x2,                                            // Debug Layer가 동작합니다. 유효성검사, 변수 일관성 검사, 에러 리포트 등으로 속도가 느려집니다.
+        D3D11_CREATE_DEVICE_SWITCH_TO_REF = 0x4,                                    // Direct3D 11에서 지원되지 않습니다.
+        D3D11_CREATE_DEVICE_PREVENT_INTERNAL_THREADING_OPTIMIZATIONS = 0x8,         // 여러 스레드가 만들어지는 것을 방지합니다. 사용하지 않는 것이 좋다는데, WARP의 작동과 관련이 있어 보입니다.
+        D3D11_CREATE_DEVICE_BGRA_SUPPORT = 0x20,                                    // Direct3D 리소스와의 Direct2D 상호 운용성에 필요합니다.
+        D3D11_CREATE_DEVICE_DEBUGGABLE = 0x40,                                      // 셰이더 디버깅에 사용할 수 있는 정보를 유지하도록 합니다.
+        D3D11_CREATE_DEVICE_PREVENT_ALTERING_LAYER_SETTINGS_FROM_REGISTRY = 0x80,   // 릴리즈 빌드를 사용하는 최종 사용자가, DirectX 제어판 등을 사용해 Debug Layer을 사용하여 모니터링 하지 못하게 합니다.
+        D3D11_CREATE_DEVICE_DISABLE_GPU_TIMEOUT = 0x100,                            // GPU Timeout이 걸리지 않도록 합니다. (2초) 
+        D3D11_CREATE_DEVICE_VIDEO_SUPPORT = 0x800
+       }
+    // https://learn.microsoft.com/ko-kr/windows/win32/api/d3d11/ne-d3d11-d3d11_create_device_flag
+    */
+
+
 #ifdef _DEBUG
     createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
+    
+    /*
+    typedef
+    enum D3D_DRIVER_TYPE
+    {
+        D3D_DRIVER_TYPE_UNKNOWN	= 0,
+        D3D_DRIVER_TYPE_HARDWARE	= ( D3D_DRIVER_TYPE_UNKNOWN + 1 ) ,     // GPU 하드웨어 가속을 사용합니다.
+        D3D_DRIVER_TYPE_REFERENCE	= ( D3D_DRIVER_TYPE_HARDWARE + 1 ) ,    // 개발 중 디버그 지원을 위한 소프트웨어로 구현된 드라이버 입니다. GPU가 없어도 동작할 수 있습니다.
+        D3D_DRIVER_TYPE_NULL	= ( D3D_DRIVER_TYPE_REFERENCE + 1 ) ,       // 렌더링 기능을 제외한 소프트웨어
+        D3D_DRIVER_TYPE_SOFTWARE	= ( D3D_DRIVER_TYPE_NULL + 1 ) ,        // 예약만 되어있고 사용하지 않습니다.
+        D3D_DRIVER_TYPE_WARP	= ( D3D_DRIVER_TYPE_SOFTWARE + 1 )          // 고성능 소프트웨어 래스터라이저인 WARP를 사용합니다. CPU가 현대적일 경우, REFERENCE 보다 훨씬 나은 성능을 보여줍니다.
+    } 	D3D_DRIVER_TYPE;
+    */
 
     D3D_DRIVER_TYPE driverTypes[] =
     {
@@ -179,8 +215,12 @@ HRESULT InitDevice()
         D3D_DRIVER_TYPE_WARP,
         D3D_DRIVER_TYPE_REFERENCE,
     };
+
     UINT numDriverTypes = ARRAYSIZE( driverTypes );
 
+    /*
+    * GPU에서 지원하는 D3D Level 입니다.
+    */
     D3D_FEATURE_LEVEL featureLevels[] =
     {
         D3D_FEATURE_LEVEL_11_1,
@@ -188,7 +228,27 @@ HRESULT InitDevice()
         D3D_FEATURE_LEVEL_10_1,
         D3D_FEATURE_LEVEL_10_0,
     };
+
 	UINT numFeatureLevels = ARRAYSIZE( featureLevels );
+
+    // driverTypes[] 에 넣어둔 D3D_DRIVER_TYPE에 따라 Device를 생성합니다. 생성 실패시 다음 단계의 D3D_DRIVER_TYPE으로 생성을 시도합니다.
+    // 해당 코드는 Hardware->WARP->Ref 순으로 시도합니다.
+    /*
+    HRESULT D3D11CreateDevice(
+        __in   IDXGIAdapter             *pAdapter,              // 디스플레이 디바이스(비디오카드)를 지정합니다. null일 경우 최초로 발견한 디바이스를 사용합니다.
+        __in   D3D_DRIVER_TYPE          DriverType,             // 생성할 타입을 넣습니다. 해당 코드는 Hardware->WARP->Ref 순으로 시도합니다.
+        __in   HMODULE                  Software,               // Software Rasterizer 가 구현된 모듈의 핸들을 넣습니다. 일반적으로는 null을 넣습니다.
+        __in   UINT                     Flags,                  // 위에서 설정한 디바이스의 레이어 플래그를 넣습니다.
+        __in   const D3D_FEATURE_LEVEL  *pFeatureLevels,        // 지원가능한 레벨에 대한 Array입니다.
+        __in   UINT                     FeatureLevels,
+        __in   UINT                     SDKVersion,             // DX SDK의 버전입니다.
+        __out  ID3D11Device             **ppDevice,             // 생성 성공된 디바이스의 포인터입니다.
+        __out  D3D_FEATURE_LEVEL        *pFeatureLevel,         // 생성 성공된 지원가능 레벨입니다.
+        __out  ID3D11DeviceContext      **ppImmediateContext    // 생성 성공된 디바이스 컨텍스트입니다.
+        );
+
+        //https://vsts2010.tistory.com/512
+    */
 
     for( UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++ )
     {
@@ -209,6 +269,11 @@ HRESULT InitDevice()
     if( FAILED( hr ) )
         return hr;
 
+    // ~ 1. Device 생성: 사용할 레이어등에 관한 Flag와 드라이버에 맞는 Device 생성
+
+    // 2. SwapChain 생성: DXGI_SWAP_CHAIN_DESC 에 명시하여 생성.
+
+    // SwapChain을 생성하는 IDXGIFactory 를 생성합니다.
     // Obtain DXGI factory from device (since we used nullptr for pAdapter above)
     IDXGIFactory1* dxgiFactory = nullptr;
     {
@@ -230,6 +295,8 @@ HRESULT InitDevice()
         return hr;
 
     // Create swap chain
+
+    // 11.1 이후 버전일 경우.
     IDXGIFactory2* dxgiFactory2 = nullptr;
     hr = dxgiFactory->QueryInterface( __uuidof(IDXGIFactory2), reinterpret_cast<void**>(&dxgiFactory2) );
     if ( dxgiFactory2 )
@@ -260,18 +327,20 @@ HRESULT InitDevice()
     }
     else
     {
+        // DXGI_SWAP_CHAIN_DESC: SwapChain(버퍼)를 만들기 위한 설정정보 구조체.
+
         // DirectX 11.0 systems
         DXGI_SWAP_CHAIN_DESC sd = {};
         sd.BufferCount = 1;
         sd.BufferDesc.Width = width;
         sd.BufferDesc.Height = height;
         sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-        sd.BufferDesc.RefreshRate.Numerator = 60;
-        sd.BufferDesc.RefreshRate.Denominator = 1;
-        sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        sd.BufferDesc.RefreshRate.Numerator = 60;   // FrameRate 분자. 
+        sd.BufferDesc.RefreshRate.Denominator = 1;  // FrameRate 분모. 59.94 Hz 일 경우는 60000/1001
+        sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;   // 해당 버퍼의 사용처.
         sd.OutputWindow = g_hWnd;       // 어느 윈도우에 투사할 것인지.
-        sd.SampleDesc.Count = 1;        // 멀티샘플링용.
-        sd.SampleDesc.Quality = 0;      // 멀티샘플링용.
+        sd.SampleDesc.Count = 1;        // 멀티샘플링에 사용할 표본갯수 1,4,8..32(MAX) 멀티샘플링: https://codingfarm.tistory.com/420, https://lipcoder.tistory.com/22
+        sd.SampleDesc.Quality = 0;      // 멀티샘플링 품질 레벨 - 0일 경우 하지 않음.
         sd.Windowed = TRUE;
 
         hr = dxgiFactory->CreateSwapChain( g_pd3dDevice, &sd, &g_pSwapChain );
@@ -285,20 +354,28 @@ HRESULT InitDevice()
     if (FAILED(hr))
         return hr;
 
+    // ~ 2. SwapChain 생성: DXGI_SWAP_CHAIN_DESC 에 명시하여 생성.
+
+    // 3. ImmediateContext 설정: Device 생성 시 획득한 ImmediateContext에 렌더타겟과 뷰포트 설정.
+
+    // SwapChain의 백버퍼를 가져옵니다.
     // Create a render target view
     ID3D11Texture2D* pBackBuffer = nullptr;
     hr = g_pSwapChain->GetBuffer( 0, __uuidof( ID3D11Texture2D ), reinterpret_cast<void**>( &pBackBuffer ) );
     if( FAILED( hr ) )
         return hr;
 
+    // SwapChain에서 얻은 백버퍼를 사용한 렌더타겟뷰를 만듭니다. (현재는 백버퍼에 아무것도 없지만...)
     hr = g_pd3dDevice->CreateRenderTargetView( pBackBuffer, nullptr, &g_pRenderTargetView );
-    pBackBuffer->Release();
+    pBackBuffer->Release();     // Q.계속 쓰게될 백버퍼를 왜 Release하는지...refCount만 하나 내리는건지 애매합니다.
     if( FAILED( hr ) )
         return hr;
 
+    // ImmediateContext 설정: 위에서 만든 렌더타겟뷰를 타겟으로.
     g_pImmediateContext->OMSetRenderTargets( 1, &g_pRenderTargetView, nullptr );
 
     // Setup the viewport
+    // 뷰포트 설정. 렌더타겟뷰의 그려질 화면영역.
     D3D11_VIEWPORT vp;
     vp.Width = (FLOAT)width;
     vp.Height = (FLOAT)height;
@@ -306,7 +383,10 @@ HRESULT InitDevice()
     vp.MaxDepth = 1.0f;
     vp.TopLeftX = 0;
     vp.TopLeftY = 0;
-    g_pImmediateContext->RSSetViewports( 1, &vp );
+    // Immediate 컨텍스트 설정: 뷰포트 설정.
+    g_pImmediateContext->RSSetViewports( 1, &vp );      // RS 는 Rasterize Stage.
+
+    // ~3. ImmediateContext 설정: Device 생성 시 획득한 ImmediateContext에 렌더타겟과 뷰포트 설정.
 
     return S_OK;
 }
